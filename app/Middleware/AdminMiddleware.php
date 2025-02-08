a989d3cc189a88281532273b2d0b5a5d9939e4c5
Gabriel Borchardt <ogborchctt@gmail.com>
2025-02-08 19:44:59 -0300

    Fix middlewares

--- app/Middleware/AdminMiddleware.php
+++ app/Middleware/AdminMiddleware.php
@@ -11,7 +11,7 @@ class AdminMiddleware implements Middleware
 {
     public function handle(Request $request): void
     {
-        if (!Auth::check() || !Auth::isBarber()) {
+        if (!Auth::check() || !Auth::isAdmin()) {
             FlashMessage::danger('Você deve ser um administrador para acessar essa página');
             $this->redirectTo(route('indexLogin'));
         }
