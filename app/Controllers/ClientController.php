fa2cc34f206cd1ebde1188aa1b76ab1bc6cf30c0
alefjuan <alefjuan88@gmail.com>
2025-02-11 21:38:38 -0300

    Criação de "meus agendamentos"; retornando para agendamento na tela home

--- app/Controllers/ClientController.php
+++ app/Controllers/ClientController.php
@@ -5,27 +5,46 @@ namespace App\Controllers;
 use Core\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Service;
+use App\Models\Scheduling;
 use Lib\Authentication\Auth;
 
 class ClientController extends Controller
 {
-    public function index():void{
+    public function index(): void
+    {
         $this->render('client/index');
     }
-    public function indexCreateSchedule():void{
+    public function indexCreateSchedule(): void
+    {
 
         $barbers = User::where(['type' => 'barber']);
-        
+
         $services = Service::all();
 
-        $this->render('client/createSchedule', compact(['barbers','services'], ['barbers','services']));
+        $this->render('client/createSchedule', compact(['barbers', 'services'], ['barbers', 'services']));
     }
 
-    public function mySchedules():void{
-        $this->render('client/mySchedules');
+    public function mySchedules()
+    {
+        $authUserId = Auth::user()->id;
+
+        $schedules = Scheduling::where(['client_id' => $authUserId]);
+
+        $formattedSchedules = [];
+        foreach ($schedules as $schedule) {
+            $formattedSchedules[] = [
+                'date' => $schedule->date,
+                'time' => date('H:i', strtotime($schedule->date)), // Extraindo a hora
+                'barber' => User::where($schedule->barber_id)->name ?? 'Desconhecido',
+                'service' => Service::where($schedule->service_id)->name ?? 'Indefinido',
+                'status' => ucfirst(string: $schedule->status),
+            ];
+        }
+
+        $this->render('client/mySchedules', compact(['formattedSchedules'], ['formattedSchedules']));
     }
-    
-    public function createSchedule():void{
+    public function createSchedule(): void
+    {
         $authUser = Auth::user()->id;
     }
 }
