d1eec299727a82ce9fbeaf218cb420d4aa7bb602
Gabriel Borchardt <ogborchctt@gmail.com>
2025-02-19 19:37:52 -0300

    Feat: Create scheduling

--- app/Controllers/ClientController.php
+++ app/Controllers/ClientController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controllers;
 
+use App\Models\BarberSchedule;
 use Core\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Service;
 use App\Models\Scheduling;
+use Core\Http\Request;
 use Lib\Authentication\Auth;
 
 class ClientController extends Controller
@@ -14,37 +16,79 @@ class ClientController extends Controller
     {
         $this->render('client/index');
     }
+
     public function indexCreateSchedule(): void
     {
-
         $barbers = User::where(['type' => 'barber']);
-
         $services = Service::all();
 
-        $this->render('client/createSchedule', compact(['barbers', 'services'], ['barbers', 'services']));
+        $barbersDisponibility = [];
+        foreach ($barbers as $barber) {
+            $barbersDisponibility[$barber->id] = $this->getBarbersDisponibility($barber->id);
+        }
+
+        $this->render('client/createSchedule', compact('barbers', 'services', 'barbersDisponibility'));
     }
 
     public function mySchedules()
     {
         $authUserId = Auth::user()->id;
-
         $schedules = Scheduling::where(['client_id' => $authUserId]);
 
         $formattedSchedules = [];
         foreach ($schedules as $schedule) {
             $formattedSchedules[] = [
                 'date' => $schedule->date,
-                'time' => date('H:i', strtotime($schedule->date)), // Extraindo a hora
-                'barber' => User::where($schedule->barber_id)->name ?? 'Desconhecido',
-                'service' => Service::where($schedule->service_id)->name ?? 'Indefinido',
-                'status' => ucfirst(string: $schedule->status),
+                'time' => date('H:i', strtotime($schedule->date)),
+                'barber' => User::where(['id' => $schedule->barber_id])->name ?? 'Desconhecido',
+                'service' => Service::where(['id' => $schedule->service_id])->name ?? 'Indefinido',
+                'status' => ucfirst($schedule->status),
             ];
         }
 
-        $this->render('client/mySchedules', compact(['formattedSchedules'], ['formattedSchedules']));
+        $this->render('client/mySchedules', compact('formattedSchedules'));
     }
-    public function createSchedule(): void
+
+    public function createScheduling(Request $request)
     {
-        $authUser = Auth::user()->id;
+        $params = $request->validate([
+            'barber_id',
+            'service_id',
+            'date',
+            'time',
+        ]);
+        $schedule = new Scheduling();
+        $schedule->client_id = Auth::user()->id;
+        $schedule->barber_id = $params['barber_id'];
+        $schedule->service_id = $params['service_id'];
+        $schedule->date = $params['date'];
+        $schedule->status = 'confirmed';
+        $schedule->disapproval_justification = ' ';
+        if($schedule->save()){
+            return json_encode([
+                'status' => 'ok',
+            ]);
+        } else {
+            echo json_encode([
+                'status' => 'error',
+            ]);
+        }
+
+    }
+    private function getBarbersDisponibility(int $barberId): array
+    {
+        $disponibility = BarberSchedule::where(['barber_id' => $barberId]);
+
+        if (empty($disponibility)) {
+            return [];
+        }
+
+        $disponibility = $disponibility[0];
+
+        return [
+            'week_days' => json_decode($disponibility->week_days) ?: [],
+            'initial_hour' => $disponibility->initial_hour,
+            'final_hour' => $disponibility->final_hour,
+        ];
     }
 }
