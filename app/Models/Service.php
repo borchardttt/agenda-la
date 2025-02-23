94b5be79920cc4891a9be46bdc75dfd2f5f807b0
Gabriel Borchardt <ogborchctt@gmail.com>
2025-02-23 14:02:58 -0300

    Add return type to NxN functions

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -3,24 +3,25 @@
 namespace App\Models;
 
 use Core\Database\ActiveRecord\BelongsTo;
+use Core\Database\ActiveRecord\BelongsToMany;
 use Lib\Validations;
 use Core\Database\ActiveRecord\Model;
 
 class Service extends Model
 {
-  protected static string $table = 'services';
-  protected static array $columns = ['name', 'price', 'time'];
+    protected static string $table = 'services';
+    protected static array $columns = ['name', 'price', 'time'];
 
 
-  public function validates(): void
-  {
-      Validations::notEmpty('name', $this);
-      Validations::notEmpty('price', $this);
-      Validations::notEmpty('time', $this);
-  }
+    public function validates(): void
+    {
+        Validations::notEmpty('name', $this);
+        Validations::notEmpty('price', $this);
+        Validations::notEmpty('time', $this);
+    }
 
-  public function barbers()
-  {
-    return $this->belongsToMany(User::class, BarberService::class, 'service_id', 'barber_id');
-  }
+    public function barbers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, BarberService::class, 'service_id', 'barber_id');
+    }
 }
